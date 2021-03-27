1ddbc7053e09b025d6f527510ec4df9139dea398
zthxxx <zthxxx.me@gmail.com>
2021-03-28 00:56:00 +0800

    ci: upgrade to actions and @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
+// https://rollupjs.org/guide/en/#configuration-files
+
 import builtins from 'builtin-modules'
 import json from '@rollup/plugin-json'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import strip from '@rollup/plugin-strip'
-import typescript from 'rollup-plugin-typescript'
-import copy from 'rollup-plugin-copy'
-import filesize from 'rollup-plugin-filesize'
+import typescript from '@rollup/plugin-typescript'
 import progress from 'rollup-plugin-progress'
+import filesize from 'rollup-plugin-filesize'
+import copy from 'rollup-plugin-copy'
 import packageJson from './package.json'
 
 
@@ -15,13 +17,15 @@ export default {
   output: {
     file: 'dist/index.js',
     format: 'cjs',
+    exports: 'auto',
   },
   watch: {
     include: 'src',
   },
   external: [
     ...builtins,
-    ...Object.keys(packageJson.dependencies),
+    ...Object.keys(packageJson.dependencies ?? {}),
+    'signale/signale',
   ],
   treeshake: {
     moduleSideEffects: false,
@@ -31,22 +35,21 @@ export default {
     copy({
       targets: [
         { src: 'src/*.sh', dest: 'dist/' },
-      ]
+      ],
     }),
     resolve(),
     json(),
     typescript(),
-    commonjs({
-      include: 'node_modules/**',
-      sourceMap: false,
-    }),
     strip({
       include: [
-        '**/*.js',
-        '**/*.ts',
+        'src/**/*.(js|ts)',
       ],
       functions: [
       ],
+    }),
+    commonjs({
+      extensions: ['.js', '.ts'],
+      sourceMap: false,
     }),
     filesize(),
   ],
